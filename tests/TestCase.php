d9112061713dbfa3ae0652137822fe6f8efe49b6
Alfonso Israel Osorio Avilez <aosorio.avilez@gmail.com>
2021-06-17 10:58:53 -0500

    Implemented revoke access api

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,11 +6,5 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication, Authenticable;
-
-    public function setUp(): void
-    {
-        parent::setUp();
-        $this->buildAuthUser();
-    }
+    use CreatesApplication;
 }
